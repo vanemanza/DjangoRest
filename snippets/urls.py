ada76bc31f1848ba7ffe7cc9c7bcf16e0b22482f
vanesa <vanemanza1@gmail.com>
2022-01-04 17:48:19 -0300

    add new views to URLconf

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,13 +3,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    path('', views.api_root),
     #path('snippets/', views.snippet_list),
     #path('snippets/<int:pk>/', views.snippet_detail),
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
-
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
