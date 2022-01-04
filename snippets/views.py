147f44cfa262024859426ce24250901349518b6e
vanesa <vanemanza1@gmail.com>
2022-01-04 17:45:02 -0300

    endpoint for the highlighted snippets

--- snippets/views.py
+++ snippets/views.py
@@ -7,7 +7,7 @@ from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-
+from rest_framework import renderers 
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -29,6 +29,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                       IsOwnerOrReadOnly]
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)                      
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
